30ac6f6910ccdab37b768edbeeb3ec7ddd0e269f
fresh5447 <doug@kosmojo.com>
2016-05-30 20:18:46 -0600

    add assignment and desc section to create new checkpoint, add new hover and active effects to submit btn

--- client/admin/PostContentView.js
+++ client/admin/PostContentView.js
@@ -5,7 +5,9 @@ export default React.createClass({
   getInitialState() {
     return {
       title: null,
-      content: null
+      content: null,
+      desc: null,
+      number: null,
     }
   },
   updateTitle(e) {
@@ -13,6 +15,12 @@ export default React.createClass({
   },
   updateContent(e) {
     this.setState({ content: e.target.value })
+  },
+  updateDesc(e) {
+    this.setState({ desc: e.target.value })
+  },
+  updateAssignment(e) {
+    this.setState({ assignment: e.target.value })
   },
   render() {
     return (
@@ -23,6 +31,10 @@ export default React.createClass({
             <fieldset className="form-group">
               <label>title</label>
               <input onChange={this.updateTitle} type="text" className="form-control" id="" placeholder="..."/>
+            </fieldset>
+            <fieldset className="form-group">
+              <label>description</label>
+              <input onChange={this.updateDesc} type="text" className="form-control" id="" placeholder="..."/>
             </fieldset>
             <fieldset className="form-group">
               <label>project</label>
@@ -38,6 +50,10 @@ export default React.createClass({
               <label>content</label>
               <textarea onChange={this.updateContent} className="form-control" id="exampleTextarea" rows="3"></textarea>
             </fieldset>
+            <fieldset className="form-group">
+              <label>assignment</label>
+              <textarea onChange={this.updateAssignment} className="form-control" id="exampleTextarea" rows="3"></textarea>
+            </fieldset>
             <div className="checkbox">
               <label>
                 <input type="checkbox"/> publish
@@ -50,6 +66,7 @@ export default React.createClass({
             <div className="">
               <h5 className="card-title">{this.state.title ? this.state.title : "No title yet..."}</h5>
               <p className="card-text"> <ReactMarkdown source={this.state.content ? this.state.content : "No content yet..."} /> </p>
+              <p className="card-text"> <ReactMarkdown source={this.state.assignment ? this.state.assignment : "No assignment yet..."} /> </p>
             </div>
           </div>
         </div>
