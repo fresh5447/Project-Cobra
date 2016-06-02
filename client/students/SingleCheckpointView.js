7f0964f32f4b50e816529c28862378584fbec5ea
fresh5447 <doug@kosmojo.com>
2016-06-01 19:54:32 -0600

    modify padding for single checkpoint view

--- client/students/SingleCheckpointView.js
+++ client/students/SingleCheckpointView.js
@@ -10,24 +10,24 @@ export default React.createClass({
       <div>
         <div className="container">
           <div className="card markdown-card">
-            <div className="card-block main-card-block">
+            <div className="card-block main-card-block ">
               <h2 className="card-title">{checkpoint.project.title}</h2>
               <NavLink to={"/dashboard/" + checkpoint.project.title }><span className="back-link">Back to Project</span></NavLink>
             </div>
             <div className="card-block">
-              <div className="card card-block">
+              <div className="card card-block inner-checkpoint-cardblock">
                 <h4 className="card-title">{checkpoint.title}</h4>
                 <p className="card-text"><ReactMarkdown source={ checkpoint.content } /></p>
               </div>
             </div>
             <div className="card-block">
-              <div className="card card-block">
+              <div className="card card-block inner-checkpoint-cardblock">
                 <h4 className="card-title">Additional Resources</h4>
                 <p className="card-text">Gonna be some more resources here ya know son?</p>
               </div>
             </div>
             <div className="card-block">
-              <div className="card card-block">
+              <div className="card card-block inner-checkpoint-cardblock">
                 <h4 className="card-title">Assignment</h4>
                 <p className="card-text"><ReactMarkdown source={ checkpoint.assignment } /></p>
               </div>
