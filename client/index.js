03a734fef925fa4d18ff2e75f9c8c45e0cd8d65e
fresh5447 <doug@kosmojo.com>
2016-06-06 21:09:30 -0600

    not much

--- client/index.js
+++ client/index.js
@@ -1,60 +1,166 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { Router, Route, browserHistory, IndexRoute } from 'react-router'
-import App from './App'
-import Repos from './Repos'
-import Repo from './Repo'
-import SingleProjectData from './students/SingleProjectData'
-import SingleCheckpointData from './students/SingleCheckpointData'
-import Home from './Home'
-import DashboardData from './students/DashboardData'
-import AdminDashData from './admin/DashboardData'
-import PostContent from './admin/PostCheckpointView'
-import PostProject from './admin/PostProjectView'
-import Login from './user/Login'
-import Signup from './user/Signup'
+import { render, findDOMNode } from 'react-dom'
+import {
+  browserHistory, Router, Route, IndexRoute, Link, withRouter
+} from 'react-router'
+import ContactStore from './ContactStore'
 
-App.displayName = 'App';
-AdminDashData.displayName = 'AdminDashData';
-SingleProjectData.displayName = 'SingleProjectData';
-DashboardData.displayName = 'DashboardData';
-SingleCheckpointData.displayName = 'SingleCheckpointData';
-PostContent.displayName = 'PostContent';
-PostProject.displayName = 'PostProject';
-Signup.displayName = 'Signup';
-Login.displayName = 'Login';
+const App = React.createClass({
+  getInitialState() {
+    return {
+      contacts: ContactStore.getContacts(),
+      loading: true
+    }
+  },
 
-require('./stylesheets/main.scss')
+  componentWillMount() {
+    ContactStore.init()
+  },
+
+  componentDidMount() {
+    ContactStore.addChangeListener(this.updateContacts)
+  },
+
+  componentWillUnmount() {
+    ContactStore.removeChangeListener(this.updateContacts)
+  },
+
+  updateContacts() {
+    if (!this.isMounted())
+      return
+
+    this.setState({
+      contacts: ContactStore.getContacts(),
+      loading: false
+    })
+  },
+
+  render() {
+    const contacts = this.state.contacts.map(function (contact) {
+      return <li key={contact.id}><Link to={`/contact/${contact.id}`}>{contact.first}</Link></li>
+    })
+
+    return (
+      <div className="App">
+        <div className="ContactList">
+          <Link to="/contact/new">New Contact</Link>
+          <ul>
+            {contacts}
+          </ul>
+        </div>
+        <div className="Content">
+          {this.props.children}
+        </div>
+      </div>
+    )
+  }
+})
+
+const Index = React.createClass({
+  render() {
+    return <h1>Address Book</h1>
+  }
+})
+
+const Contact = withRouter(
+  React.createClass({
+
+    getStateFromStore(props) {
+      const { id } = props ? props.params : this.props.params
+
+      return {
+        contact: ContactStore.getContact(id)
+      }
+    },
+
+    getInitialState() {
+      return this.getStateFromStore()
+    },
+
+    componentDidMount() {
+      ContactStore.addChangeListener(this.updateContact)
+    },
+
+    componentWillUnmount() {
+      ContactStore.removeChangeListener(this.updateContact)
+    },
+
+    componentWillReceiveProps(nextProps) {
+      this.setState(this.getStateFromStore(nextProps))
+    },
+
+    updateContact() {
+      if (!this.isMounted())
+        return
+
+      this.setState(this.getStateFromStore())
+    },
+
+    destroy() {
+      const { id } = this.props.params
+      ContactStore.removeContact(id)
+      this.props.router.push('/')
+    },
+
+    render() {
+      const contact = this.state.contact || {}
+      const name = contact.first + ' ' + contact.last
+      const avatar = contact.avatar || 'http://placecage.com/50/50'
+
+      return (
+        <div className="Contact">
+          <img height="50" src={avatar} key={avatar} />
+          <h3>{name}</h3>
+          <button onClick={this.destroy}>Delete</button>
+        </div>
+      )
+    }
+  })
+)
+
+const NewContact = withRouter(
+  React.createClass({
+
+    createContact(event) {
+      event.preventDefault()
+
+      ContactStore.addContact({
+        first: findDOMNode(this.refs.first).value,
+        last: findDOMNode(this.refs.last).value
+      }, (contact) => {
+        this.props.router.push(`/contact/${contact.id}`)
+      })
+    },
+
+    render() {
+      return (
+        <form onSubmit={this.createContact}>
+          <p>
+            <input type="text" ref="first" placeholder="First name" />
+            <input type="text" ref="last" placeholder="Last name" />
+          </p>
+          <p>
+            <button type="submit">Save</button> <Link to="/">Cancel</Link>
+          </p>
+        </form>
+      )
+    }
+  })
+)
+
+const NotFound = React.createClass({
+  render() {
+    return <h2>Not found</h2>
+  }
+})
 
 render((
   <Router history={browserHistory}>
     <Route path="/" component={App}>
-      <IndexRoute component={Home}/>
-
-      <Route path="/admin-dashboard" component={AdminDashData}/>
-
-      <Route path="/dashboard" component={DashboardData}/>
-
-      <Route path="/dashboard/:projectName" component={SingleProjectData}/>
-
-      <Route path="/projects/:checkpointName" component={SingleCheckpointData}/>
-
-      <Route path="/add-checkpoint" component={PostContent}/>
-      <Route path="/add-project" component={PostProject}/>
-      <Route path="/signup" component={Signup}/>
-      <Route path="/login" component={Login}/>
+      <IndexRoute component={Index} />
+      <Route path="contact/new" component={NewContact} />
+      <Route path="contact/:id" component={Contact} />
+      <Route path="*" component={NotFound} />
     </Route>
   </Router>
 ), document.getElementById('app'))
-
-
-
-
-
-{ /* 
-      nested route example
-
-      <Route path="/repos" component={Repos}>
-        <Route path="/repos/:userName/:repoName" component={Repo}/>
-      </Route>
-  */ }
