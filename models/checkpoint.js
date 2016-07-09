617cc9c61e9a46ea0bfd0aba9a0e9f724f77cf0f
fresh5447 <doug@kosmojo.com>
2016-07-09 16:16:08 -0600

    order checkpoints successfull

--- models/checkpoint.js
+++ models/checkpoint.js
@@ -1,15 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'),
+  autoIncrement = require('mongoose-auto-increment'),
+  Schema = mongoose.Schema;
 
-const Schema = mongoose.Schema;
+var connection = mongoose.createConnection('mongodb://localhost:27017/lms');
+autoIncrement.initialize(connection);
 
 const CheckpointSchema = new Schema({
-  title:      String,
-  desc:       String,
-  number:     Number,
-  publish:    Boolean,
-  content:    String,
+  title: String,
+  desc: String,
+  number: Number,
+  publish: Boolean,
+  content: String,
   assignment: String,
-  module: { type: mongoose.Schema.Types.ObjectId, ref: 'Module' }
+  module: { type: mongoose.Schema.Types.ObjectId, ref: 'Module' },
+  order: Number
 });
+
+CheckpointSchema.plugin(autoIncrement.plugin, { model: 'Checkpoint', field: 'order' });
 
 module.exports = mongoose.model('Checkpoint', CheckpointSchema);
