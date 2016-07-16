1f720304a36729571fcb03365b885e79d012e659
fresh5447 <doug@kosmojo.com>
2016-07-15 19:21:15 -0600

    post new resource form

--- client/resources/PostResourceData.js
+++ client/resources/PostResourceData.js
@@ -1,13 +1,61 @@
 import React from 'react';
+import PostResourceForm from './PostResourceForm';
+import { browserHistory } from 'react-router';
 
 class PostResourceData extends React.Component {
-  render() {
-    return (
-      <div>
-        <h3>Form For New Resources</h3>
-      </div>
-    )
+  constructor(props) {
+    super(props);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      title: null,
+      link: null,
+      desc: null,
+    };
+
   }
+
+  onFieldChange(fieldName, fieldValue) {
+    const newState = {};
+    newState[fieldName] = fieldValue;
+    this.setState(newState);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const data = {
+      title: this.state.title,
+      desc: this.state.desc,
+      link: this.state.link
+    };
+    $.ajax({
+      url: '/api/v1/resources',
+      method: 'POST',
+      data
+    }).done((d) => {
+      this.context.sendNotification("Resource Created");
+      const path = '/resources/all'
+      browserHistory.push(path);
+    });
+  }
+
+  render() {
+    return <PostResourceForm
+    onFieldChange={(...args) => this.onFieldChange(...args)}
+    handleSubmit={this.handleSubmit}
+    title={this.state.title}
+    title={this.state.link}
+    link={this.state.link}
+    assignment={this.state.assignment}
+    />;
+  }
+
 }
+
+PostResourceData.displayName = PostResourceData;
+PostResourceData.contextTypes = {
+  sendNotification: React.PropTypes.func.isRequired
+};
 
 export default PostResourceData;
