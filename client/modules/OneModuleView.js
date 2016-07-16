703d7f138c026a8ef90e7c1e39d530a806697a9b
fresh5447 <doug@kosmojo.com>
2016-07-16 13:11:43 -0600

    style module view page

--- client/modules/OneModuleView.js
+++ client/modules/OneModuleView.js
@@ -5,8 +5,8 @@ const Module = (props) => {
   const cps = props.module.checkpoints && props.module.checkpoints.length > 0 ?
     props.module.checkpoints.map((item) => {
     return (
-      <div className="card markdown-card">
-        <div className="card-block main-card-block">
+      <div className="card markdown-card modules-card">
+        <div className="card-block main-card-block modules-card-header">
           <h4 className="card-title">{ item.title} </h4>
           <p>Complete???</p>
           <NavLink className="nav-link" to={"/modules/" + props.mId + "/checkpoints/" + item._id}>View</NavLink>
@@ -14,18 +14,17 @@ const Module = (props) => {
       </div>);
   }) : <div>No CPS</div>;
   return (
-    <div><h4>Module</h4>
-      <div className="card markdown-card">
-        <div className="card-block main-card-block">
-          <h4 className="card-title">Title: { props.module.title} </h4>
-          <p className="pull-right"> module #1 </p>
+    <div>
+      <div className="card markdown-card all-modules-card">
+        <div className="card-block modules-card-body">
+          <h4 className="card-title">{ props.module.title} </h4>
         </div>
-        <div className="card-block">
-          <p> Description: { props.module.desc}</p>
+        <div className="card-block modules-card-body">
+          <p>30/40 checkpoints complete </p>
+          <p>{ props.module.desc}</p>
 
         </div>
       </div>
-      <h4> Checkpoints </h4>
       <div className="checkpoints-flex">
         {cps}
       </div>
