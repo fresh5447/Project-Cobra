f2f008983c28f137842ebf0d3c3b69b231f61556
fresh5447 <doug@kosmojo.com>
2016-07-22 12:41:17 -0600

    add header to all modules view

--- client/modules/ModulesList.js
+++ client/modules/ModulesList.js
@@ -8,6 +8,14 @@ const ModulesList = (props) => {
 
   return (
     <div>
+    <div className="card markdown-card all-modules-card">
+      <div className="card-block modules-card-body">
+        <h4 className="card-title">FullStack Web Developement </h4>
+      </div>
+      <div className="card-block modules-card-body">
+        <p>0/2 modules complete </p>
+      </div>
+    </div>
       <div className="modules-flex">
         { mods }
       </div>
