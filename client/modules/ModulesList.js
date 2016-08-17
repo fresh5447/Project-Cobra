50a699690c1cf989e59b8bac0bdc38ae185a4b49
fresh5447 <doug@kosmojo.com>
2016-08-17 16:46:40 -0600

    reordering the things

--- client/modules/ModulesList.js
+++ client/modules/ModulesList.js
@@ -5,12 +5,12 @@ import NavLink from '../widgets/NavLink';
 const ModulesList = (props) => {
   window.m = props.modules;
   let coms = 0;
-  const mods = props.modules.map((mod) => {
+  const mods = props.modules.sort(function(a,b) {return (a.order > b.order) ? 1 : ((b.order > a.order) ? -1 : 0);} ).map((mod) => {
     if (mod.complete === true) {
       coms += 1;
     }
     return <ModulesCard live={mod.live ? mod.live : false} key={mod._id} complete={mod.complete} title={mod.title} id={mod._id} desc={mod.desc} checkpoints={mod.checkpoints} />;
-  }).sort(function(a,b) {return (a.order > b.order) ? 1 : ((b.order > a.order) ? -1 : 0);} );
+  })
 
   const cats = props.modules.map((mod) => {
     return <li className="list-group-item"><NavLink to={'/modules/' + mod._id } key={mod._id} complete={mod.complete} title={mod.title} id={mod._id}> {mod.title} </NavLink>{mod.complete ? <i className="fa fa-check-square-o pull-right"></i> : null}</li>
