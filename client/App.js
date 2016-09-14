a0a290ca217fbf80c95bbcdb1546e9bd4eddab3b
fresh5447 <doug@kosmojo.com>
2016-09-14 15:48:42 -0600

    delete old client directory and old index, move new widgets into new folder

--- client/App.js
+++ client/App.js
@@ -3,19 +3,18 @@ import NavBar from './widgets/NavBar';
 import Notifier from './context/Notifier';
 import GetUser from './context/GetUser';
 import Feedback from './context/Feedback';
-import Signup from './user/Signup';
+import Login from './user/UserSignin';
 
 
 class App extends React.Component {
   render() {
-    const h = <div> Hello </div>;
     return (
       <div>
         <GetUser>
           <Notifier>
             <Feedback>
                 <NavBar>
-                    {this.props.children ? this.props.children : <Signup/> }
+                    {this.props.children ? this.props.children : <Login /> }
                 </NavBar>
             </Feedback>
           </Notifier>
